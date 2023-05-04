e7cc5bb802a4c8fae80d73a20cb8c8784db96409
Biruk Yimer <birukyimer@Biruks-MacBook-Pro.local>
2023-05-04 14:01:16 -0400

    second commit (like/unlike working)

--- blogtutorial/blog/urls.py
+++ blogtutorial/blog/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import Index
+from django.urls import path, include
+from .views import Index, DetailArticleView, LikeArticle
 
 urlpatterns = [
     path('', Index.as_view(), name = 'index'),
+    path('tinymce/', include('tinymce.urls')),
+    path('<int:pk>/', DetailArticleView.as_view(), name = 'detail_article'),
+    path('<int:pk>/like', LikeArticle.as_view(), name='like_article')
+
 ]
